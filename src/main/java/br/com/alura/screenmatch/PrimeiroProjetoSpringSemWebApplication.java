d811375aac178fac3d78dfdb6ecf08e4245fcc4b
Gremiao <gremiao@gmail.com>
2025-01-16 09:39:12 -0300

    Melhorias de codigo.

--- src/main/java/br/com/alura/screenmatch/PrimeiroProjetoSpringSemWebApplication.java
+++ src/main/java/br/com/alura/screenmatch/PrimeiroProjetoSpringSemWebApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,14 +14,8 @@ public class PrimeiroProjetoSpringSemWebApplication implements CommandLineRunner
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		Principal principal	= new Principal();
+		principal.exibeMenu();
 
-//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-//		System.out.println(json);
-
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dadosSerie);
 	}
 }
